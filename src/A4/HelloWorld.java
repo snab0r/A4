885fa3ecbb1aaa9cbfc7ec5a02c5b76a163bba8b
Samantha Nabor <samantha.nabor@gmail.com>
2017-02-07 17:29:41 -0800

    Added name to comment

--- src/A4/HelloWorld.java
+++ src/A4/HelloWorld.java
@@ -3,7 +3,7 @@ package A4;
 import java.util.Date;
 
 /**
- * Created by saman on 2/7/2017.
+ * Created by Samantha Nabor on 2/7/2017.
  */
 public class HelloWorld {
     public static void main(String[] args)
