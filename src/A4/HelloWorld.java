88a78a7729942eb862749e33a12c21b9a46b6fef
Samantha Nabor <samantha.nabor@gmail.com>
2017-02-07 17:22:09 -0800

    add

--- src/A4/HelloWorld.java
+++ src/A4/HelloWorld.java
@@ -4,4 +4,8 @@ package A4;
  * Created by saman on 2/7/2017.
  */
 public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
 }
