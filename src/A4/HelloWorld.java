66bf0997eba98e6ab49b26c3b981ba16eab4cbdc
Samantha Nabor <samantha.nabor@gmail.com>
2017-02-07 17:25:01 -0800

    Added date

--- src/A4/HelloWorld.java
+++ src/A4/HelloWorld.java
@@ -1,5 +1,7 @@
 package A4;
 
+import java.util.Date;
+
 /**
  * Created by saman on 2/7/2017.
  */
@@ -7,5 +9,6 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+        System.out.println(new Date());
     }
 }
